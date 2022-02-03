0c7cb741dad3b7e77f186dd73c124c3f87e8ee67
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-02-02 21:28:00 -0300

    typeorm setup

---
+++ src/shared/infra/typeorm/index.ts
@@ -0,0 +1,16 @@
+import { Connection, createConnection, getConnectionOptions } from "typeorm";
+import "reflect-metadata";
+
+export default async (host = "database"): Promise<Connection> => {
+    const defaultOptions = await getConnectionOptions();
+
+    return createConnection(
+        Object.assign(defaultOptions, {
+            host: process.env.NODE_ENV === "test" ? "localhost" : host,
+            database:
+                process.env.NODE_ENV === "test"
+                    ? "roomly_test"
+                    : defaultOptions.database,
+        })
+    );
+};
