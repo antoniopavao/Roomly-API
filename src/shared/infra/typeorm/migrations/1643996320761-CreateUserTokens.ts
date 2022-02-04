0a895d4f7d7a811158c5e14723695ee5c428ca71
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-02-04 14:39:21 -0300

    creating userTokens entity and migration

---
+++ src/shared/infra/typeorm/migrations/1643996320761-CreateUserTokens.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateUserTokens1643996320761 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
